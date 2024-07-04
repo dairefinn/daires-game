b5718963422eba4d7505c7c7580b8aeea312019d
Daire Finn <dmanfinn@gmail.com>
2024-07-05 00:18:11 +0100

    Bullet fires at the correct angle

--- Entities/Weapons/WeaponFirearm.gd
+++ Entities/Weapons/WeaponFirearm.gd
@@ -5,6 +5,7 @@ class_name WeaponFirearm;
 var resourceBullet = load("res://Entities/Weapons/Projectile/Bullet.tscn");
 @export var bulletEmitter: Node3D;
 
+var BULLET_FORCE = 20;
 var BASE_AMMO_CAPACITY = 10;
 var ammoLeft = 0;
 
@@ -33,23 +34,32 @@ func spawnBullet(_bulletEmitter: Node3D) -> void:
 	if (_bulletEmitter == null):
 		print_debug("Bullet emitter is null");
 		return;
-	
+
 	if (ammoLeft == 0):
 		print_debug("You have no ammo left");
 		return;
-	
+
 	var newBullet = resourceBullet.instantiate();
 	Globals.add_projectile.emit(newBullet);
-	
-	print_debug("Bullet emitter rotation:")
-	print_debug(_bulletEmitter.global_rotation)
-	
-	newBullet.global_position = _bulletEmitter.global_position;
-	newBullet.global_rotation = _bulletEmitter.global_rotation;
-	newBullet.gravity_scale = 0;
-	# TODO: Apply a forward force once the rotation is set corrrectly
-	#newBullet.linear_velocity.z = -1;
-	
+
+	var oldScale = newBullet.scale;
+
+	# Set the bullet's global transform to match the emitter's exactly
+	newBullet.global_transform = _bulletEmitter.global_transform;
+	newBullet.scale = oldScale;
+
+	# # Debugging to confirm the match
+	# print_debug("Emitter Global Transform: ", _bulletEmitter.global_transform)
+	# print_debug("Bullet Global Transform: ", newBullet.global_transform)
+
+	# This is used to freeze the bullet in the air for testing the bullet spawn angles
+	# newBullet.gravity_scale = 0;
+
+	# Assuming the bullet's forward direction is along its local Z-axis
+	# This line might not be necessary if the bullet's physics or movement logic
+	# automatically handles forward motion based on its orientation
+	newBullet.apply_central_impulse(newBullet.transform.basis.z.normalized() * -BULLET_FORCE);
+
 	ammoLeft -= 1;
 
 
